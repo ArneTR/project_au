b3814d16f5a586baa8f59a1a0b9202eb98efffcc
Arne Tarara <arne@datafuse.de>
2014-09-02 21:54:03 +0200

    Removed duplicate includes

--- project_au/network.hpp
+++ project_au/network.hpp
@@ -11,15 +11,7 @@
   #include <arpa/inet.h>
   #include <netdb.h>
   #include <errno.h>
-  #include <stdio.h>
   #include <stdlib.h>
-  #include <unistd.h>
-  #include <string.h>
-  #include <sys/types.h>
-  #include <sys/socket.h>
-  #include <netinet/in.h>
-  #include <netdb.h> 
-
 
 #endif
 
