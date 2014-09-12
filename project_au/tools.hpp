d5248836a02ddbb02ada4c61c54649b7f15c2a52
Arne Tarara <arne@datafuse.de>
2014-09-12 21:01:04 +0200

    defined must be in tools in order for compile to work

--- project_au/tools.hpp
+++ project_au/tools.hpp
@@ -17,6 +17,27 @@
     double beta;
     double gamma;
   }SIMPLE_POSE;
+
+  #define APP_SOCKET_PORT 11113
+  #define ROBOT_SOCKET_PORT 11114
+
+  #define PENCIL_LENGTH 146.0 // should be 146.0 later on
+
+  #define MARKER_SIZE 46.0 // mm
+  #define MARKER
+  #define MARKER_RESOLUTION 5
+  #define MARKER_BLACK_MARGIN_RESOLUTION 1.0
+  #define MARKER_BLACK_MARGIN 6.0
+
+  #define MARKER_WHITE_MARGIN_RESOLUTION 1.0
+  #define MARKER_WHITE_MARGIN 6.0
+
+  #define ROBOT_MARKER 0
+  #define ROBOT_MARKER_SIZE 103.0
+
+  #define TIME_AVERAGE_POSES 5.0
+
+  #define BRIGHTNESS_MARGIN 40
 #endif
 
 SIMPLE_POSE getSimplePose(Pose p);
